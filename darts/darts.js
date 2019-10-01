a0c3059fd1cd0570cdb068e0350e52f983428a17
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-01 20:53:38 +0100

    Add darts exercise

---
+++ darts/darts.js
@@ -0,0 +1,13 @@
+export const solve = (x, y) => {
+  const radius = Math.sqrt(x**2 + y**2)
+
+  if (radius  <= 1) {
+    return 10;
+  } else if (radius  <= 5) {
+    return 5;
+  } else if (radius  <= 10) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
