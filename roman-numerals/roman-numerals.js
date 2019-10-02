369a794937333ed04437ebd4f093f1aac64d1770
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-02 18:30:08 +0100

    Add roman numerals exercise

---
+++ roman-numerals/roman-numerals.js
@@ -0,0 +1,35 @@
+export const toRoman = (num) => {
+  const ROMAN_NUMERALS =  {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX", 
+    5: "V",
+    4: "IV",
+    1: "I"
+  }
+
+  let number = num;
+  let romanNumerals = "";
+
+  for (let num of Object.keys(ROMAN_NUMERALS).reverse().map(n => Number(n))) {
+
+    let occurence = Math.floor(number / num)
+
+    if (occurence >= 1) {
+      for (let i = 0; i < occurence; i++) {
+        romanNumerals += ROMAN_NUMERALS[num]
+      }
+    }
+
+    number = number % num;
+  }
+
+  return romanNumerals;
+};
