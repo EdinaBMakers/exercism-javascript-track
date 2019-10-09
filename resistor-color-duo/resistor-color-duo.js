628320677baa89261ec17355783d2e6e37c0a254
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-09 16:56:54 +0100

    Refactor

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,8 @@
 const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
 export const value = colors => {
-  const colorsResistorValues = colors.slice(0,2).map(colorCode);
-  return colorsResistorValues[0] * 10 + colorsResistorValues[1];
+  const [firstColor, secondColor] = colors;
+  return colorCode(firstColor) * 10 + colorCode(secondColor);
 };
 
 const colorCode = color => COLORS.indexOf(color);
