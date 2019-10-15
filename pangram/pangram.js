ecbf4f248e02edb1ad0e189cb4e8e6aae07cd5bf
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-15 19:21:10 +0100

    Refactor

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,7 +1,7 @@
 const ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 export const isPangram = (str) => {
-  if (str === '') {
+  if (str.length < ALPHABET.length) {
     return false;
   }
 
