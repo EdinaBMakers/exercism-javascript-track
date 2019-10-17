64149cdb8472cd0643e7811ac74df6520753997b
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-17 10:56:25 +0100

    Refactor

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,11 +1,11 @@
-const ALPHABET = [...'abcdefghijklmnopqrstuvwxyz'];
+const ALPHABET_LENGTH = 26;;
 
 export const isPangram = (str) => {
-  if (str.length < ALPHABET.length) {
+  if (str.length < ALPHABET_LENGTH) {
     return false;
   }
 
   const uniqueLCaseLetters = [...new Set(str.toLowerCase().match(/[a-z]/g))];
 
-  return uniqueLCaseLetters.length === ALPHABET.length;
+  return uniqueLCaseLetters.length === ALPHABET_LENGTH;
 };
