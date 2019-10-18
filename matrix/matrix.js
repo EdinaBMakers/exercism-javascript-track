14702380979703dfc9b2bda8861175943532f3cc
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-18 19:26:15 +0100

    Add matrix exercise

---
+++ matrix/matrix.js
@@ -0,0 +1,17 @@
+export class Matrix {
+  constructor(str) {
+    this.str = str;
+  }
+
+  get rows() {
+    const rowStr = this.str.split('\n')
+    return rowStr.map(row => row.split(' ').map(num => Number(num)));
+  }
+
+  get columns() {
+    const rows = this.rows;
+    const colIndices = rows[0].map((_, index) => index);
+    
+    return colIndices.map(colIndex => rows.map(row => row[colIndex]));
+  }
+}
