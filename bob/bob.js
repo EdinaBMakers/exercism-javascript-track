63109bea0c7ab489a8dcb32c4fc09e54962c7db5
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-21 19:41:01 +0100

    Refactor

--- bob/bob.js
+++ bob/bob.js
@@ -1,21 +1,24 @@
 export const hey = (message) => {
   const trimmedMessage = message.trim();
-  const lastChar = trimmedMessage[trimmedMessage.length - 1];
 
-  if (trimmedMessage === '') {
+  if (isSayingNothing(trimmedMessage)) {
     return 'Fine. Be that way!';
   }
   
-  if (trimmedMessage === trimmedMessage.toUpperCase() 
-      && trimmedMessage.match(/[a-zA-z]+/) != null) {
-    return lastChar === '?' 
+  if (isYelling(trimmedMessage) && hasLetter(trimmedMessage)) {
+    return isAsking(trimmedMessage) 
              ? 'Calm down, I know what I\'m doing!' 
              : 'Whoa, chill out!';
   }
   
-  if (lastChar === '?') {
+  if (isAsking(trimmedMessage)) {
     return 'Sure.';
   }
 
   return 'Whatever.';
 };
+
+const isAsking = message => message[message.length - 1] === '?';
+const isYelling = message => message === message.toUpperCase();
+const hasLetter = message => message.match(/[a-zA-z]+/) != null;
+const isSayingNothing = message => message === '';
